33629c7c851f03c51226b930129ad915e120d6cb
Brad Barnhill <barnhill.brad@heb.com>
2023-11-01 22:03:49 -0500

    zoomable lib

--- pdfviewer/build.gradle.kts
+++ pdfviewer/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.zoomable)
 }
 
 val dokkaOutputDir = "$buildDir/dokka"
