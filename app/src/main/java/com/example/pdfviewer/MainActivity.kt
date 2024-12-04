dc7a55e36241fffb6e3fc1d8a080b2523fcce501
Brad Barnhill <bradfordbarnhill@gmail.com>
2024-12-03 19:00:15 -0600

    update dependencies

--- app/src/main/java/com/example/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/example/pdfviewer/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
@@ -19,20 +21,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             PdfViewerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.LightGray
-                ) {
-                    PdfViewer(
-                        url = "https://raw.githubusercontent.com/barnhill/ComposePdfViewer/main/app/src/main/assets/sample.pdf",
-                        loadingContent = {
-                            Text(
-                                modifier = Modifier.align(Alignment.Center),
-                                text = "Loading..."
-                            )
-                        }
-                    )
-                    /*PdfViewer(file = FileUtil.getTestFile(this))*/
+                Scaffold { padding ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(top = padding.calculateTopPadding()),
+                        color = Color.LightGray
+                    ) {
+                        PdfViewer(
+                            url = "https://raw.githubusercontent.com/barnhill/ComposePdfViewer/main/app/src/main/assets/sample.pdf",
+                            loadingContent = {
+                                Text(
+                                    modifier = Modifier.align(Alignment.Center),
+                                    text = "Loading..."
+                                )
+                            }
+                        )
+                        /*PdfViewer(file = FileUtil.getTestFile(this))*/
+                    }
                 }
             }
         }
--- pdfviewer/src/main/java/com/pnuema/android/pdfviewer/DefaultFileRetriever.kt
+++ pdfviewer/src/main/java/com/pnuema/android/pdfviewer/DefaultFileRetriever.kt
@@ -1,10 +1,8 @@
 package com.pnuema.android.pdfviewer
 
 import android.content.Context
-import androidx.collection.LruCache
 import com.google.android.gms.net.CronetProviderInstaller
 import com.google.net.cronet.okhttptransport.CronetInterceptor
-import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
@@ -20,24 +18,20 @@ class DefaultFileRetriever(
     private val targetTempFile: File,
 ): PDFFileRetriever, AutoCloseable {
 
-    private val client: OkHttpClient
+    private val client: OkHttpClient = okHttpClient ?: run {
+        CronetProviderInstaller.installProvider(context)
+        val engine = CronetEngine.Builder(context.applicationContext)
+            .enableBrotli(true)
+            .setStoragePath(context.cacheDir.absolutePath)
+            .enableHttpCache(CronetEngine.Builder.HTTP_CACHE_DISK, 1024 * 1024 * 25) // 25mb cache
+            .build()
+        val cronetInterceptor = CronetInterceptor.newBuilder(engine).build()
 
-    init {
-        client = okHttpClient ?: run {
-            CronetProviderInstaller.installProvider(context)
-            val engine = CronetEngine.Builder(context.applicationContext)
-                .enableBrotli(true)
-                .setStoragePath(context.cacheDir.absolutePath)
-                .enableHttpCache(CronetEngine.Builder.HTTP_CACHE_DISK, 1024 * 1024 * 25) // 25mb cache
-                .build()
-            val cronetInterceptor = CronetInterceptor.newBuilder(engine).build()
-
-            OkHttpClient
-                .Builder()
-                .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC })
-                .addInterceptor(cronetInterceptor)
-                .build()
-        }
+        OkHttpClient
+            .Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC })
+            .addInterceptor(cronetInterceptor)
+            .build()
     }
 
     override fun from(source: String): File? {
